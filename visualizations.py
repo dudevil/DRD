fe2ed59cfd5951c19117d8da0437cff60d28f5ff
Sergey HPC <ovcharenko.ser@gmail.com>
2015-05-12 22:47:48 +0300

    Add visualization of image parts important for classification as in this paper: http://arxiv.org/abs/1311.2901 .

--- visualizations.py
+++ visualizations.py
@@ -1,9 +1,12 @@
 __author__ = 'dudevil'
 
+import numpy as np
+import theano
+from itertools import product
 import matplotlib.pyplot as plt
 from matplotlib import gridspec
 from matplotlib.cm import Greys_r
-import numpy as np
+from utils import load_network
 
 def show_weights(layer, bias=True, filt=-1, color=False):
     W = layer.W.get_value()
@@ -56,3 +59,59 @@ def plot_confusion_matrix(conf_matrix, labels=(0, 1, 2, 3, 4), normalize=True,
             plt.text(x + x_offset, y + y_offset, "%.2f" % cm[y,x])
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
+
+
+def occlusion_heatmap(net, image, level=4, square_length=7):
+    """
+    Determines which parts of the image are important for the network classification
+    Blackens square regions in the image and measures target class probability
+    See this paper: http://arxiv.org/abs/1311.2901
+
+    :param net: a path to a pickeled network or a (net, output) tuple
+    :param image: an image to test, this function assumes the images has been preprocessed
+        in the same manner as during net training, otherwise the results are unpredictable
+    :param level: measure agains this class (currently only class 4 is supported)
+    :param square_length: size of the square which is blacked out in the image
+
+    :return: an array the same size as image with target class probabilities
+    """
+    if isinstance(net, basestring):
+        net, output = load_network(net)
+    else:
+        net, output = net
+    img = image.copy()
+    shape = img.shape
+    # assume the first layer is InputLayer
+    net_shape = net[0].get_output_shape()
+    batch_size = net_shape[0]
+    assert shape == net_shape[1:]
+    # allocate network input variable
+    x_shared = theano.shared(np.zeros(net_shape, dtype=theano.config.floatX), borrow=True)
+    # allocate array for the heatmap
+    heat_array = np.zeros(shape[1:])
+    pad = square_length // 2
+    # this will contain images with occluded regions
+    x_occluded = np.zeros((shape[1] * shape[2], net_shape[1], shape[1], shape[2]),
+                          dtype=img.dtype)
+    # occlude image regions
+    for i, j in product(*map(range, shape[1:])):
+        x_padded = np.pad(img, ((0, 0), (pad, pad), (pad, pad)), 'constant')
+        x_padded[:, i:i + square_length, j:j + square_length] = 0.
+        x_occluded[i * shape[1] + j, ...] = x_padded[:, pad:-pad, pad:-pad]
+
+    predict_proba = theano.function(
+        [],
+        output.get_output(x_shared, deterministic=True)[:, level - 1],
+    )
+    n_occluded = len(x_occluded)
+    probas = np.zeros(n_occluded, dtype=theano.config.floatX)
+    # get the probabilities for occluded images
+    for b in xrange(n_occluded / batch_size):
+        batch_slice = slice(b * batch_size, (b + 1) * batch_size)
+        x_shared.set_value(x_occluded[batch_slice], borrow=True)
+        probas[batch_slice] = predict_proba()
+    #assign probabilities to heat_map
+    for i, j in product(*map(range, shape[1:])):
+        heat_array[i, j] = probas[i * shape[1] + j]
+
+    return heat_array
