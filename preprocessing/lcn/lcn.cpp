12889c5dc8fda5645c8f5132e3621615be02451f
rasim <brotherofken@gmail.com>
2015-06-18 23:11:07 +0300

    bugfix, script update

--- preprocessing/lcn/lcn.cpp
+++ preprocessing/lcn/lcn.cpp
@@ -21,17 +21,19 @@ cv::Mat lcn_color(cv::Mat const& img, int const ksize, bool const normalize_glob
 {
 	assert(!img.empty() && img.channels() == 3);
 	assert(ksize > 0);
-	img.convertTo(img, CV_32FC3, 1.0 / 255.0);
-	return apply_lcn(img, ksize, normalize_global);
+	cv::Mat img_converted;
+	img.convertTo(img_converted, CV_32FC3, 1.0 / 255.0);
+	return apply_lcn(img_converted, ksize, normalize_global) * 255.0;
 }
 
 cv::Mat lcn_gray(cv::Mat const& img, int const ksize, bool const normalize_global)
 {
 	assert(!img.empty() && img.channels() == 1);
 	assert(ksize > 0);
-	cv::cvtColor(img, img, CV_BGR2GRAY);
-	img.convertTo(img, CV_32FC1, 1.0 / 255.0);
-	return apply_lcn(img, ksize, normalize_global);
+	cv::Mat img_converted;
+	cv::cvtColor(img, img_converted, CV_BGR2GRAY);
+	img_converted.convertTo(img_converted, CV_32FC1, 1.0 / 255.0);
+	return apply_lcn(img_converted, ksize, normalize_global) * 255.0;
 }
 
 cv::Mat lcn_yuv(cv::Mat const& img, int const ksize, bool const normalize_global)
@@ -41,33 +43,34 @@ cv::Mat lcn_yuv(cv::Mat const& img, int const ksize, bool const normalize_global
 
 	cv::Mat img_yuv;
 	cv::cvtColor(img, img_yuv, CV_BGR2YUV);
-	img_yuv.convertTo(img_yuv, CV_32FC3, 1.0 / 255.0);
+	img_yuv.convertTo(img_yuv, CV_32FC3);
 
 	// Split channels
-	std::vector<cv::Mat> channels(img.channels());
-	cv::split(img, channels);
+	std::vector<cv::Mat> channels(img_yuv.channels());
+	cv::split(img_yuv, channels);
 
 	// LCN on Y channel
-	channels[0] = normalize_local_contrast(channels[0], ksize);
+	channels[0] = normalize_local_contrast(channels[0] / 255.0, ksize);
 	
 	// GCN on Y channel if requested
 	channels[0] = normalize_global ? normalize_global_contrast(channels[0]) : channels[0];
 
 	// Y channel now has unit variance, so scale it to [0, 1] range
 	channels[0] += gaussian_99_percentile;
-	channels[0].convertTo(channels[0], channels[0].type(), 1 / (2 * gaussian_99_percentile));
+	channels[0].convertTo(channels[0], channels[0].type(), 255. / (2. * gaussian_99_percentile));
 
 	cv::merge(channels, img_yuv);
 
 	//img_normed1.convertTo(img_normed1, CV_8U)
 	cv::Mat result;
+	img_yuv.convertTo(img_yuv, CV_8UC3);
 	cv::cvtColor(img_yuv, result, CV_YUV2BGR);
 	return result;
 }
 
 static const cv::String keys =
 	"{in      |       | input                             }"
-	"{iout    |       | output                            }"
+	"{out    |       | output                            }"
 	"{ksize   | 21    | kernel size                       }"
 	"{gray    | false | convert to gray before processing }"
 	"{gcn     | false | do global contrast normalization  }"
@@ -84,7 +87,7 @@ int main(int const argc, char const* const argv[])
 	}
 
 	// read command line
-	std::string const input_filename  = cli.get<std::string>("int");
+	std::string const input_filename  = cli.get<std::string>("in");
 	std::string const output_filename = cli.get<std::string>("out");
 	int const k_size             = cli.get<int>("ksize");
 
@@ -104,6 +107,6 @@ int main(int const argc, char const* const argv[])
 
 	cv::Mat const result = do_lcn(img, k_size, normalize_global);
 
-	cv::imwrite(output_filename, result * 255.0);
+	cv::imwrite(output_filename, result);
 	return EXIT_SUCCESS;
 }
--- preprocessing/lcn/run_lcn.sh
+++ preprocessing/lcn/run_lcn.sh
@@ -7,17 +7,23 @@ set_name=${1:-train}
 kernel_size=${2:-17}
 
 # If you want to convert image to grayscale
-#gray=${3:0}
+gray=${3:-false}
+gcn=${4:-false}
+yuv=${5:-false}
+
+name_gray=$([ ${gray} == "true" ] && echo "_gray" || echo "" )
+name_gcn=$([ ${gcn} == "true" ] && echo "_gcn"  || echo "" )
+name_yuv=$([ ${yuv} == "true" ] && echo "_yuv"  || echo "" )
 
 # Calculate LCN images
 # You already SHOULD HAVE file lists to this moment
 
 cd ${set_name}
 
-for lst in ../${set_name}/_list_*;
+for lst in ../${set_name}_list_*;
 do
     echo $lst;
-    (while read line; do echo $line; ../lcn ${line%jpeg}png ${line%.jpeg}_lcn.png ${kernel_size} 0; done < $lst ) &
+    (while read line; do echo $line; outname=${line%.jpeg}_lcn_${kernel_size}${name_gray}${name_gcn}${name_yuv}.png; ../lcn -in=${line%jpeg}png -out=${outname} -ksize=${kernel_size} -gray=${gray} -yuv=${yuv} -gcn=${gcn}; done < $lst ) &
 done;
 wait
 
