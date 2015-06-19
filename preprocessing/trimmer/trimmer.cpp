93bdcd362e95ec41ea542d7ec2f0d3d02aff0d09
rasim <brotherofken@gmail.com>
2015-06-19 23:54:05 +0300

    inner square image trimming, experimental code in derivatives filter DON'T USE!

--- preprocessing/trimmer/trimmer.cpp
+++ preprocessing/trimmer/trimmer.cpp
@@ -1,10 +1,38 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <cassert>
 
 #include <opencv2\opencv.hpp>
 
 #include <iputils\trim.hpp>
+
+
+// Output width = sqrt(2.) * / 2 * input.width
+cv::Mat get_inner_square(cv::Mat const& img) {
+	assert(img.cols == img.rows);
+
+	int const side_size = img.cols;
+	float angle = 45;
+	cv::Point rect_center(side_size / 2, side_size / 2);
+	cv::Size rect_size(std::sqrt(2.) * side_size / 2., std::sqrt(2.) * side_size / 2.);
+
+	cv::RotatedRect rect = cv::RotatedRect(rect_center, rect_size, angle);
+
+	if (rect.angle < -45.) {
+		angle += 90.0;
+		cv::swap(rect_size.width, rect_size.height);
+	}
+
+	// get the rotation matrix
+	cv::Mat M = getRotationMatrix2D(rect.center, angle, 1.0);
+	// perform the affine transformation
+	cv::Mat rotated, cropped;
+	warpAffine(img, rotated, M, img.size(), cv::INTER_CUBIC);
+	// crop the resulting image
+	getRectSubPix(rotated, rect_size, rect.center, cropped);
+	return cropped;
+}
 
 int main(int const argc, char const* const argv[])
 {
@@ -12,12 +40,14 @@ int main(int const argc, char const* const argv[])
 	if (argc != 4) {
 		std::cout
 			<< "usage:" << std::endl
-			<< "./trimmer <input file> <output file> <output width>" << std::endl;
+			<< "./trimmer <input file> <output file> <output width> <trim_to_inner_square>" << std::endl
+			<< "If <trim_to_inner_square> set to 1, result width = sqrt(2.) * / 2 * <output width>, image rotated by 45 degrees!" << std::endl;
 		return EXIT_FAILURE;
 	}
 	std::string const input_filename(argv[1]);
 	std::string const output_filename(argv[2]);
 	unsigned const output_side_size = std::stoi(argv[3]);
+	bool take_inner_square = true;
 
 	// read image, resize, split
 	cv::Mat3b const img = cv::imread(input_filename);
@@ -27,6 +57,14 @@ int main(int const argc, char const* const argv[])
 	// resize & write output
 	cv::Mat3b img_resized;
 	cv::resize(img_trimmed, img_resized, cv::Size(output_side_size, img_trimmed.rows * double(output_side_size) / img_trimmed.cols));
+
+	// make image square
+	int const rows_remained = output_side_size - img_resized.rows;
+	int const top = rows_remained / 2;
+	int const bottom = rows_remained / 2 + rows_remained % 2;
+	cv::copyMakeBorder(img_resized, img_resized, top, bottom, 0, 0, cv::BORDER_CONSTANT, cv::Scalar::all(0));
+
+	img_resized = take_inner_square ? get_inner_square(img_resized) : img_resized;
 
 	cv::imwrite(output_filename, img_resized);
 
